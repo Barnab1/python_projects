0d0a66b3e982cf4bfb94d241fbc6f606cb644307
AGUEH Barnabé Mahugnon <barnabeagueh@gmail.com>
2025-04-09 15:29:37 +0000

    First phase: being able to make friend from closest friend

--- findMutualFriend/project.py
+++ findMutualFriend/project.py
@@ -1,4 +1,16 @@
 #python 3
 """
-That is just a simple purpose with this 
+That is just a simple purpose with this project: Findind friends to suggest based on users' friend
 """
+friends = {
+    'jean':['Amyr', 'Yvon', 'lambert'],
+    'Amyr':['jean', 'Mathieu', 'Thomas'],
+    'Lucien':['lambert','Euphrasie','Miracle']
+}
+
+suggestedFriend = []
+
+for friend in friends:
+    for newFriend in friends[friend]:
+        suggestedFriend.append(newFriend) 
+print(suggestedFriend)
