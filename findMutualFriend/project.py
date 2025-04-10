ec3e0f629c8c7b88ec2ffe0cdac7fdf51a29e9a2
AGUEH Barnabé Mahugnon <barnabeagueh@gmail.com>
2025-04-10 12:50:18 +0000

    Achieved functionality: make friends from core values

--- findMutualFriend/project.py
+++ findMutualFriend/project.py
@@ -18,10 +18,28 @@ def makeFriendsFromFriends():
     return suggestedFriend
 
 def makeFriendsBasedOnCharacter():
+
+    """
+        Return array of friends based oneself's core values
+    """
     coreQuality = ["upright", "disciplined", "honest"]
 
     friends = {
         "jean":{"respectful","positive", "angry", "talkative"},
         "Innocent":{"talkative", "hard worker", "disciplined", "honest"},
-        "Afiavi": {"hard worker", "ambitious","angry"}
+        "Afiavi": {"hard worker", "ambitious","angry", "honest"}
     }
+
+    friendsFromQuality = []
+    
+    for friend in friends:
+        
+        for quality in friends[friend]:
+            
+            if quality in coreQuality:
+                friendsFromQuality.append(friend)
+
+    friendsFromQuality = set(friendsFromQuality)
+
+    return friendsFromQuality
+print(makeFriendsBasedOnCharacter())
