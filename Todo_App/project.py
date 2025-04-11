6227b03b5d1e7e3233f5d967d84e2dca4729ab9f
AGUEH Barnabé Mahugnon <barnabeagueh@gmail.com>
2025-04-11 09:06:26 +0000

    with Gemini's help, I ask questions about how to approach the App

--- Todo_App/project.py
+++ Todo_App/project.py
@@ -3,3 +3,28 @@
 #Goal: Create a command-line or basic GUI (using Tkinter) application to manage tasks.
 #Skills: Variables, lists, loops, conditional statements, basic file I/O (to save/load tasks).
 #Progression: Add features like task prioritization, due dates, and task completion tracking.
+
+"""
+Functionality and user interaction
+
+    - actions performed by user: add, view, complete as done
+
+    - way user interact with application: Commmand line input
+
+    - type of input : text prompts
+
+    - type of output:  formatted list
+
+    - Tasks should be ordered by description priority ?
+
+        -Priority hare are ('Must do':m,'should do':s,'could do':c, 'Will not do': w)
+
+    - How will the user identify which task to mark as complete or delete? (By number ?, description ?)
+
+    - What feedback should the user receive after each action? (Confirmation messages when everythin is fine, error message
+        when there is a mistake)
+
+Data structures and Storage
+
+    - How will the tasks be stored in memory while the application is running? (List of dictionaries)
+"""
