5abf98ef719ee6dde2cabfa9d486dc605583d9bb
AGUEH Barnabé Mahugnon <barnabeagueh@gmail.com>
2025-05-05 10:38:10 +0000

    started the Graphical User Interface in python

--- Todo_App/test_date.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-# Get current date and time
-now = datetime.now()
-print("Current date and time:", now)
-
-# Get current date
-today = datetime.today()
-print("Current date:", today)
